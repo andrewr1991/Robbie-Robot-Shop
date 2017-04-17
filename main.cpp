ddcc49cafa7048c331ab5be4882a3085c98ef6cb
Andrew <fridout106@gmail.com>
2017-04-17 10:40:56 -0500

    Second build with fltk

--- main.cpp
+++ main.cpp
@@ -19,8 +19,122 @@ Fl_Window *win;
 
 using namespace std;
 
+void create_robot_torso(Fl_Widget* w, void* p);
+void cancel_robot_torso(Fl_Widget* w, void* p);
+void create_robot_head(Fl_Widget* w, void* p);
+void cancel_robot_head(Fl_Widget* w, void* p);
 void create_robot_arm(Fl_Widget* w, void* p);
 void cancel_robot_arm(Fl_Widget* w, void* p);
+void create_robot_locomotor(Fl_Widget* w, void* p);
+void cancel_robot_locomotor(Fl_Widget* w, void* p);
+void create_robot_battery(Fl_Widget* w, void* p);
+void cancel_robot_battery(Fl_Widget* w, void* p);
+
+class Robot_torso_Dialog {
+	public:
+		Robot_torso_Dialog() {
+			dialog = new Fl_Window(340, 340, "New Robot Arm");
+				t_name = new Fl_Input(120, 10, 210, 25, "Name:");
+				t_name->align(FL_ALIGN_LEFT);
+				
+				t_battery_compartments = new Fl_Input(120, 40, 210, 25, "Battery Compartments (1-3):");
+				t_battery_compartments->align(FL_ALIGN_LEFT);
+				
+				t_max_arms = new Fl_Input(120, 70, 210, 25, "Max arms (1-3):");
+				t_max_arms->align(FL_ALIGN_LEFT);
+				
+				t_create = new Fl_Return_Button(145, 100, 120, 25, "Create Arm");
+				t_create->callback((Fl_Callback *)create_robot_torso, 0);
+				
+				t_cancel = new Fl_Button(270, 100, 60, 25, "Cancel");
+				t_cancel->callback((Fl_Callback *)cancel_robot_torso, 0);
+				dialog->end();
+				dialog->set_non_modal();
+		}
+	
+		void show() {dialog->show();}
+		void hide() {dialog->hide();}
+		string name() {return t_name->value();}
+		string battery_compartments() {return t_battery_compartments->value();}
+		string max_arms() {return t_max_arms->value();}
+	
+	private:
+		Fl_Window *dialog;
+		Fl_Input *t_name;
+		Fl_Input *t_battery_compartments;
+		Fl_Input *t_max_arms;
+		Fl_Return_Button *t_create;
+		Fl_Button *t_cancel;
+};
+
+Robot_torso_Dialog *robot_torso_dlg;
+
+void create_robot_torso(Fl_Widget* w, void* p) {
+	string name = robot_torso_dlg->name();
+	string battery_compartments = robot_torso_dlg->battery_compartments();
+	string max_arms = robot_torso_dlg->max_arms();
+	
+	/*
+	Patron pat (name, phone);
+	robot_arm_dlg->hide();
+	library.add_patron(pat);
+	*/
+	
+}
+
+void cancel_robot_torso(Fl_Widget* w, void* p) {
+	robot_torso_dlg->hide();
+}
+
+class Robot_head_Dialog {
+	public:
+		Robot_head_Dialog() {
+			dialog = new Fl_Window(340, 340, "New Robot Head");
+				h_name = new Fl_Input(120, 10, 210, 25, "Name:");
+				h_name->align(FL_ALIGN_LEFT);
+				
+				h_power = new Fl_Input(120, 40, 210, 25, "Power (25-100):");
+				h_power->align(FL_ALIGN_LEFT);
+				
+				h_create = new Fl_Return_Button(145, 100, 120, 25, "Create Head");
+				h_create->callback((Fl_Callback *)create_robot_head, 0);
+				
+				h_cancel = new Fl_Button(270, 100, 60, 25, "Cancel");
+				h_cancel->callback((Fl_Callback *)cancel_robot_head, 0);
+				dialog->end();
+				dialog->set_non_modal();
+		}
+	
+		void show() {dialog->show();}
+		void hide() {dialog->hide();}
+		string name() {return h_name->value();}
+		string power() {return h_power->value();}
+	
+	private:
+		Fl_Window *dialog;
+		Fl_Input *h_name;
+		Fl_Input *h_power;
+		Fl_Return_Button *h_create;
+		Fl_Button *h_cancel;
+};
+
+Robot_head_Dialog *robot_head_dlg;
+
+void create_robot_head(Fl_Widget* w, void* p) {
+	string name = robot_head_dlg->name();
+	string power = robot_head_dlg->power();
+	
+	/*
+	Patron pat (name, phone);
+	robot_arm_dlg->hide();
+	library.add_patron(pat);
+	*/
+	
+}
+
+void cancel_robot_head(Fl_Widget* w, void* p) {
+	robot_head_dlg->hide();
+}
 
 
 class Robot_arm_Dialog {
@@ -33,7 +147,7 @@ class Robot_arm_Dialog {
 				a_max_power = new Fl_Input(120, 40, 210, 25, "Max power (1-3):");
 				a_max_power->align(FL_ALIGN_LEFT);
 				
-				a_create = new Fl_Return_Button(145, 70, 120, 25, "Create Arm");
+				a_create = new Fl_Return_Button(145, 100, 120, 25, "Create Arm");
 				a_create->callback((Fl_Callback *)create_robot_arm, 0);
 				
 				a_cancel = new Fl_Button(270, 100, 60, 25, "Cancel");
@@ -59,8 +173,8 @@ Robot_arm_Dialog *robot_arm_dlg;
 
 void create_robot_arm(Fl_Widget* w, void* p) {
 	string name = robot_arm_dlg->name();
+	string max_power = robot_arm_dlg->max_power();
 	
-	string result;
 	/*
 	Patron pat (name, phone);
 	robot_arm_dlg->hide();
@@ -73,13 +187,127 @@ void cancel_robot_arm(Fl_Widget* w, void* p) {
 	robot_arm_dlg->hide();
 }
 
-int main() {
+class Robot_locomotor_Dialog {
+	public:
+		Robot_locomotor_Dialog() {
+			dialog = new Fl_Window(340, 340, "New Robot Head");
+				l_name = new Fl_Input(120, 10, 210, 25, "Name:");
+				l_name->align(FL_ALIGN_LEFT);
+				
+				l_max_power = new Fl_Input(120, 40, 210, 25, "Max Power (25-100):");
+				l_max_power->align(FL_ALIGN_LEFT);
+				
+				l_create = new Fl_Return_Button(145, 100, 120, 25, "Create Head");
+				l_create->callback((Fl_Callback *)create_robot_locomotor, 0);
+				
+				l_cancel = new Fl_Button(270, 100, 60, 25, "Cancel");
+				l_cancel->callback((Fl_Callback *)cancel_robot_locomotor, 0);
+				dialog->end();
+				dialog->set_non_modal();
+		}
+	
+		void show() {dialog->show();}
+		void hide() {dialog->hide();}
+		string name() {return l_name->value();}
+		string max_power() {return l_power->value();}
+	
+	private:
+		Fl_Window *dialog;
+		Fl_Input *l_name;
+		Fl_Input *l_max_power;
+		Fl_Return_Button *l_create;
+		Fl_Button *l_cancel;
+};
+
+Robot_locomotor_Dialog *robot_locomotor_dlg;
+
+void create_robot_locomotor(Fl_Widget* w, void* p) {
+	string name = robot_locomotor_dlg->name();
+	string max_power = robot_locomotor_dlg->max_power();
+	
+	/*
+	Patron pat (name, phone);
+	robot_arm_dlg->hide();
+	library.add_patron(pat);
+	*/
+	
+}
+
+void cancel_robot_locomotor(Fl_Widget* w, void* p) {
+	robot_locomotor_dlg->hide();
+}
+
+class Robot_battery_Dialog {
+	public:
+		Robot_battery_Dialog() {
+			dialog = new Fl_Window(340, 340, "New Robot Head");
+				b_name = new Fl_Input(120, 10, 210, 25, "Name:");
+				b_name->align(FL_ALIGN_LEFT);
+				
+				b_power_available = new Fl_Input(120, 40, 210, 25, "Power Available (25-100):");
+				b_power_available->align(FL_ALIGN_LEFT);
+				
+				b_create = new Fl_Return_Button(145, 100, 120, 25, "Create Head");
+				b_create->callback((Fl_Callback *)create_robot_battery, 0);
+				
+				b_cancel = new Fl_Button(270, 100, 60, 25, "Cancel");
+				b_cancel->callback((Fl_Callback *)cancel_robot_battery, 0);
+				dialog->end();
+				dialog->set_non_modal();
+		}
+	
+		void show() {dialog->show();}
+		void hide() {dialog->hide();}
+		string name() {return b_name->value();}
+		string power_available() {return b_power_available->value();}
+	
+	private:
+		Fl_Window *dialog;
+		Fl_Input *b_name;
+		Fl_Input *b_power_available;
+		Fl_Return_Button *b_create;
+		Fl_Button *b_cancel;
+};
+
+Robot_battery_Dialog *robot_battery_dlg;
+
+void create_robot_battery(Fl_Widget* w, void* p) {
+	string name = robot_battery_dlg->name();
+	string power_available = robot_battery_dlg->power_available();
+	
+	/*
+	Patron pat (name, phone);
+	robot_arm_dlg->hide();
+	library.add_patron(pat);
+	*/
+	
+}
+
+void cancel_robot_battery(Fl_Widget* w, void* p) {
+	robot_battery_dlg->hide();
+}
+
+/*************** Execute Command Function ****************/
+
+int execute_cmd(string command) {
+	if (command == "1") {
+		robot_torso_dlg = new Robot_torso_Dialog{};
+		robot_torso_dlg->show();
+		return Fl::run();
+	}
+	else if (command == "2") {
+		robot_arm_dlg = new Robot_arm_Dialog{};
+		robot_arm_dlg->show();
+		return Fl::run();
+	}
+}
+
+int show_menu() {
 	
 	string command;
 	string msg;
 	
 	while (true) {
-	
 		msg = "=======================\n";
 		msg += "C1325 Robbie Robot Shop\n";
 		msg += "=======================\n\n";
@@ -99,20 +327,20 @@ int main() {
 		fl_message_icon()->label("L");
 		command = fl_input(msg.c_str());
 		
-		if (command == "1") {
-			robot_arm_dlg = new Robot_arm_Dialog{};
-			robot_arm_dlg->show();
-			return Fl::run();
-		}
+		execute_cmd(command);
 	}
+}
 	
-	const int x = 360;
-	const int y = 220;
+int main() {
+show_menu();
 
-	win = new Fl_Window{ x, y, "Robbie Robot Shop" };
-	win->color(FL_WHITE);
+const int x = 360;
+const int y = 220;
 
-	win->end();
-	win->show();
-	return(Fl::run());
+win = new Fl_Window{ x, y, "Library Management System" };
+win->color(FL_WHITE);
+
+win->end();
+win->show();
+return(Fl::run());
 }
